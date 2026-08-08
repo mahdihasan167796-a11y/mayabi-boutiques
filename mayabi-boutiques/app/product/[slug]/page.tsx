0abe622f60c7a6f5fb30ec5bc95e52a3249df079
mahdihasan167796-a11y <mahdihasan167796@gmail.com>
2026-08-08 15:13:00 +0600

    fix: decode slug parameter for bangla text

--- mayabi-boutiques/app/product/[slug]/page.tsx
+++ mayabi-boutiques/app/product/[slug]/page.tsx
@@ -5,7 +5,9 @@ import { getProductBySlug } from "@/lib/products";
 import { ProductDetailClient } from "./product-detail-client";
 
 export default async function ProductPage({ params }: { params: { slug: string } }) {
-  const product = await getProductBySlug(params.slug);
+  const decodedSlug = decodeURIComponent(params.slug);
+  const product = await getProductBySlug(decodedSlug);
+
   if (!product) notFound();
 
   return <ProductDetailClient product={product} />;
