a7f18aacbe3c0cf38a1e7f2347288355591faeb5
mahdihasan167796-a11y <mahdihasan167796@gmail.com>
2026-09-14 10:52:07 +0600

    Update social-icons.tsx

--- components/social-icons.tsx
+++ components/social-icons.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
-export function SocialIcons() {
+export function FacebookIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <div className="flex space-x-4">
-      {/* Social links placeholder */}
-    </div>
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+    </svg>
   );
 }
 
-export default SocialIcons;
+export function InstagramIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+      <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+    </svg>
+  );
+}
+
+export function TiktokIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 1 1-5.2-1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V5.9a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 3 12a6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V9.05a8.27 8.27 0 0 0 4.92 1.59V7.19a4.85 4.85 0 0 1-1-.5z"/>
+    </svg>
+  );
+}
+
+export function MessengerIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M12 2C6.477 2 2 6.145 2 11.258c0 2.91 1.455 5.51 3.734 7.178V22l3.41-1.875c.91.252 1.876.39 2.856.39 5.523 0 10-4.145 10-9.257C22 6.145 17.523 2 12 2zm1.03 12.333l-2.584-2.757-5.045 2.757 5.55-5.89 2.646 2.757 4.98-2.757-5.547 5.89z" />
+    </svg>
+  );
+}
+
+export default function SocialIcons() {
+  return (
+    <div className="flex space-x-4">
+      <FacebookIcon className="w-5 h-5" />
+      <InstagramIcon className="w-5 h-5" />
+      <TiktokIcon className="w-5 h-5" />
+      <MessengerIcon className="w-5 h-5" />
+    </div>
+  );
+}
