094a533f1f72ecfaf541b60701c4ecec886c7756
mahdihasan167796-a11y <mahdihasan167796@gmail.com>
2026-09-14 09:56:59 +0600

    Create social-icons.tsx

---
+++ components/social-icons.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function SocialIcons() {
+  return (
+    <div className="flex space-x-4">
+      {/* Social links placeholder */}
+    </div>
+  );
+}
+
+export default SocialIcons;
